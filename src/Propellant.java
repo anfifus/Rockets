4825dc5ecf9c0e4ccada1b312de322f02168344a
Aleix Alvarez <anfifus@gmail.com>
2022-03-04 12:56:24 +0100

    Commit for fase 3

--- src/Propellant.java
+++ src/Propellant.java
@@ -1,16 +1,33 @@
 public class Propellant {
-    private int power;
+    private final int maxPower;
+    private int actualPower;
 
-    public Propellant(int power) throws Exception {
-        checkPower(power);
-        this.power = power;
+    public Propellant(int maxPower) throws Exception {
+        checkPower(maxPower);
+        this.maxPower = maxPower;
+        this.actualPower = 0;
     }
 
-    private void checkPower(int power) throws  Exception{
-        if(power <= 0) throw new Exception("The propellant must have power");
+    private void checkPower(int maxPower) throws  Exception{
+        if(maxPower <= 0) throw new Exception("The propellant must have maximum power");
     }
 
-    public int getPower() {
-        return power;
+    public int getMaxPower() {
+        return maxPower;
+    }
+
+
+    public void setActualPower(int actualPower) {
+        this.actualPower += actualPower;
+        if(this.actualPower > maxPower){
+            this.actualPower = maxPower;
+        }
+        else if(this.actualPower < 0){
+            this.actualPower = 0;
+        }
+    }
+
+    public int getActualPower() {
+        return actualPower;
     }
 }
--- src/Rocket.java
+++ src/Rocket.java
@@ -2,42 +2,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rocket {
-    private String code;
-    private List<Propellant>propellantList = new ArrayList<>();
+    private final String code;
+    private List<Propellant> propellantList = new ArrayList<>();
+
     public Rocket(String code) throws Exception {
-        checkProblems(code);
+        checkCode(code);
         this.code = code;
     }
 
-    private void checkProblems(String code) throws Exception {
-        checkCode(code);
-    }
-
-    private void checkPropellant(int numOfPropellant) throws Exception{
-        if(numOfPropellant<=0) throw new Exception("The rocket must have propellant");
-    }
-
-    private void checkCode(String code) throws Exception{
-        if(code.length() != 8) throw new Exception("The format of the code is incorrect");
+    private void checkCode(String code) throws Exception {
+        if (code.length() != 8) throw new Exception("The format of the code is incorrect");
     }
 
     public String getCode() {
         return code;
     }
 
-    public String getNumOfPropellant() {
+    public String getNumOfPropellantWithMaxPower() {
         StringBuilder resultToShow = new StringBuilder();
-        int counter = 0;
-        for (Propellant currentPropellant:propellantList) {
-            resultToShow.append(currentPropellant.getPower()).append(" ");
+        for (Propellant currentPropellant : propellantList) {
+            resultToShow.append(currentPropellant.getMaxPower()).append(" ");
         }
         return resultToShow.toString();
     }
 
-    public void addPropellant(int[] propellant)throws Exception {
-        for (int currentPropellantPotency:propellant) {
+    public int getThePowerOfAllPropellant() {
+        int resultToShow = 0;
+        for (Propellant currentPropellant : propellantList) {
+            resultToShow+= currentPropellant.getActualPower();
+        }
+        return resultToShow;
+    }
+
+    public void addPropellant(int[] propellant) throws Exception {
+        for (int currentPropellantPotency : propellant) {
             propellantList.add(new Propellant(currentPropellantPotency));
         }
-
+    }
+    public void increasePower(){
+        for (Propellant currentPropellant:propellantList) {
+            currentPropellant.setActualPower(10);
+        }
+    }
+    public void decreasePower(){
+        for (Propellant currentPropellant:propellantList) {
+            currentPropellant.setActualPower(-10);
+        }
     }
 }
